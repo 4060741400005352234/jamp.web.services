dd1964a3c8e524212cb49da1712a538bfa2c0e96
4060741400005352234 <Slony6ko>
2016-03-24 19:22:01 +0600

    DB connection issue solved

--- JampWSJpa/src/jamp/processor/TicketProcessor.java
+++ JampWSJpa/src/jamp/processor/TicketProcessor.java
@@ -14,48 +14,66 @@ import jamp.model.TicketState;
 import jamp.util.JPAUtil;
 
 public class TicketProcessor {
-	
+
 	private EntityManagerFactory emf;
-	private EntityManager em;
-	
+
 	public TicketProcessor() {
 		emf = JPAUtil.getEntityManagerFactory();
-		em = emf.createEntityManager();
 	}
 
 	public long reservTicket(Ticket ticket, Passanger passanger) {
-		ticket.setState(TicketState.RESERVED);
-		ticket.setPassanger(passanger);
-		passanger.setTicket(ticket);
-		new PassangerDao(em).save(passanger);
+		EntityManager em = emf.createEntityManager();
+		try {
+			ticket.setState(TicketState.RESERVED);
+			ticket.setPassanger(passanger);
+			passanger.setTicket(ticket);
+			new PassangerDao(em).save(passanger);
+		} finally {
+			em.close();
+		}
 		return ticket.getId();
 	}
 
 	public Ticket getTicket(long id) {
-		return new TicketDao(em).get(id);
+		EntityManager em = emf.createEntityManager();
+		try {
+			return new TicketDao(em).get(id);
+		} finally {
+			em.close();
+		}
 	}
 
 	public void payForTicket(long id) throws TicketStateException, NonExistentTicketException {
-		AbstractDao<Ticket> ticketDao = new TicketDao(em);
-		Ticket ticket = ticketDao.get(id);
-		if (ticket == null) {
-			throw new NonExistentTicketException("Ticket with id " + id + " is not exist.");
+		EntityManager em = emf.createEntityManager();
+		try {
+			AbstractDao<Ticket> ticketDao = new TicketDao(em);
+			Ticket ticket = ticketDao.get(id);
+			if (ticket == null) {
+				throw new NonExistentTicketException("Ticket with id " + id + " is not exist.");
+			}
+			if (TicketState.RESERVED.equals(ticket.getState())) {
+				ticket.setState(TicketState.PAID);
+				ticketDao.update(ticket);
+			} else {
+				throw new TicketStateException("Ticket was not reserved.");
+			}
+		} finally {
+			em.close();
 		}
-		if (TicketState.RESERVED.equals(ticket.getState())) {
-			ticket.setState(TicketState.PAID);
-			ticketDao.update(ticket);
-		} else {
-			throw new TicketStateException("Ticket was not reserved.");
-		}		
 	}
 
 	public void returnTicket(long id) throws NonExistentTicketException {
-		Ticket ticket = new TicketDao(em).get(id);
-		if (ticket == null) {
-			throw new NonExistentTicketException("Ticket with id " + id + " is not exist.");
+		EntityManager em = emf.createEntityManager();
+		try {
+			Ticket ticket = new TicketDao(em).get(id);
+			if (ticket == null) {
+				throw new NonExistentTicketException("Ticket with id " + id + " is not exist.");
+			}
+			Passanger passenger = ticket.getPassanger();
+			new PassangerDao(em).remove(passenger);
+		} finally {
+			em.close();
 		}
-		Passanger passenger = ticket.getPassanger();
-		new PassangerDao(em).remove(passenger);
 	}
 
 }
